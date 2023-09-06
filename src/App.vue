116d0b5facf6992919d44f30b4d6c91358d03bbc
jsebasrojas29 <83875794+jsebasrojas29@users.noreply.github.com>
2023-09-06 11:52:34 -0500

    inicio2

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Stats from "./components/Stats.vue";
+import Problematic from "./components/problematic.vue";
+import Solution from "./components/solution.vue";
+import Benefits from "./components/benefits.vue";
+import Testimonials from "./components/testimonials.vue";
+import Characteristics from "./components/characteristics.vue";
+import Faqs from "./components/faqs.vue";
+import Action from "./components/action.vue";
+</script>
+
+<template>
+  <div class="container mx-auto mt-20">
+    <Header />
+  </div>
+  <Stats />
+  <Problematic />
+  <Solution />
+  <Benefits />
+  <Testimonials />
+  <Characteristics />
+  <Faqs />
+  <Action />
+</template>
---
+++ src/components/faqs.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="bg-white py-24 sm:py-32 text-center">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-6xl lg:mx-0">
+        <h2
+          class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center mb-6"
+        >
+          Preguntas frecuentes
+        </h2>
+        <Disclosure>
+          <DisclosureButton class="py-2 mx-auto">
+            Is team pricing available?
+          </DisclosureButton>
+          <DisclosurePanel class="text-gray-500">
+            Yes! You can purchase a license that you can share with your entire
+            team.
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+      <div class="mx-auto max-w-6xl lg:mx-0">
+        <Disclosure>
+          <DisclosureButton class="py-2 mx-auto">
+            Is team pricing available?
+          </DisclosureButton>
+          <DisclosurePanel class="text-gray-500">
+            Yes! You can purchase a license that you can share with your entire
+            team.
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+      <div class="mx-auto max-w-6xl lg:mx-0">
+        <Disclosure>
+          <DisclosureButton class="py-2 mx-auto">
+            Is team pricing available?
+          </DisclosureButton>
+          <DisclosurePanel class="text-gray-500">
+            Yes! You can purchase a license that you can share with your entire
+            team.
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+      <div class="mx-auto max-w-6xl lg:mx-0">
+        <Disclosure>
+          <DisclosureButton class="py-2 mx-auto">
+            Is team pricing available?
+          </DisclosureButton>
+          <DisclosurePanel class="text-gray-500">
+            Yes! You can purchase a license that you can share with your entire
+            team.
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+</script>
